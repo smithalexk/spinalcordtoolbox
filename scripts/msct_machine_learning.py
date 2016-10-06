2bde3a794df200318865d6518333e52cc52aba96
Benjamin De Leener <jackbdl@gmail.com>
2016-10-05 23:10:08 -0400

    DEV: patches extraction
    Former-commit-id: b93290da0a4ce0b0bded52c32de3effe87da85ce
    Former-commit-id: 57280476b0a3b9e90602f02c7b0ea8da1d2a16b1

--- scripts/msct_machine_learning.py
+++ scripts/msct_machine_learning.py
@@ -17,43 +17,95 @@ from msct_image import Image
 import numpy as np
 
 
-def extract_patches_from_image(fname_raw_images, fname_gold_images, patches_coordinates, patch_info, verbose=1):
+def extract_patches_from_image(path_dataset, fname_raw_images, fname_gold_images, patches_coordinates, patch_info, verbose=1):
     # input: list_raw_images
     # input: list_gold_images
     # output: list of patches. One patch is a pile of patches from (first) raw images and (second) gold images. Order are respected.
-    result = []
+
+    # TODO: apply rotation of the image to take patches in planes event when doing the extraction in physical space
+
+    patch_size = patch_info['patch_size']  # [int, int]
+    patch_pixdim = patch_info['patch_pixdim']  # {'axial': [float, float], 'sagittal': [float, float], 'frontal': [float, float]}
+
+    raw_images = [Image(path_dataset + fname) for fname in fname_raw_images]
+    gold_images = [Image(path_dataset + fname) for fname in fname_gold_images]
+
+    patches_raw, patches_gold = [], []
     for k in range(len(patches_coordinates)):
         ind = [patches_coordinates[k][0], patches_coordinates[k][1], patches_coordinates[k][2]]
+        patches_raw, patches_gold = [], []
 
-        # Transform voxel coordinates to physical coordinates to deal with different resolutions
-        # 1. transform ind to physical coordinates
-        # already done!
-        #ind_phys = image_file.transfo_pix2phys([ind])[0]
-        # 2. create grid around ind  - , ind_phys[2]
-        grid_physical = np.mgrid[ind[0] - patch_size / 2:ind[0] + patch_size / 2,
-                        ind[1] - patch_size / 2:ind[1] + patch_size / 2]
-        # 3. transform grid to voxel coordinates
-        coord_x = grid_physical[0, :, :].ravel()
-        coord_y = grid_physical[1, :, :].ravel()
-        coord_physical = [[coord_x[i], coord_y[i], ind_phys[2]] for i in range(len(coord_x))]
-        grid_voxel = np.array(image_file.transfo_phys2continuouspix(coord_physical))
-        # 4. interpolate image on the grid, deal with edges
-        patch = np.reshape(image_file.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
-                                                 interpolation_mode=1), (patch_size, patch_size))
+        if 'axial' in patch_pixdim:
+            range_x = np.linspace(ind[0] - (patch_size[0] / 2.0) * patch_pixdim['axial'][0], ind[0] + (patch_size[0] / 2.0) * patch_pixdim['axial'][0], patch_size[0])
+            range_y = np.linspace(ind[1] - (patch_size[1] / 2.0) * patch_pixdim['axial'][1], ind[1] + (patch_size[1] / 2.0) * patch_pixdim['axial'][1], patch_size[1])
+            coord_x, coord_y = np.meshgrid(range_x, range_y)
+            coord_x = coord_x.ravel()
+            coord_y = coord_y.ravel()
+            coord_physical = [[coord_x[i], coord_y[i], ind[2]] for i in range(len(coord_x))]
 
-        if verbose == 2:
-            import matplotlib.pyplot as plt
-            fig, ax = plt.subplots()
-            cax = ax.imshow(patch, cmap='gray')
-            cbar = fig.colorbar(cax, ticks=[0, 255])
-            plt.show()
+            for raw_image in raw_images:
+                grid_voxel = np.array(raw_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(raw_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                        interpolation_mode=1), (patch_size, patch_size))
+                patches_raw.append(np.expand_dims(patch, axis=0))
 
-        if patch.shape[0] == patch_size and patch.shape[1] == patch_size:
-            result.append(np.expand_dims(patch, axis=0))
-    if len(result) != 0:
-        return np.concatenate(result, axis=0)
-    else:
-        return None
+            for gold_image in gold_images:
+                grid_voxel = np.array(gold_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(gold_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                         interpolation_mode=1), (patch_size, patch_size))
+                patches_gold.append(np.expand_dims(patch, axis=0))
+
+        if 'sagittal' in patch_pixdim:
+            range_x = np.linspace(ind[0] - (patch_size[0] / 2.0) * patch_pixdim['sagittal'][0], ind[0] + (patch_size[0] / 2.0) * patch_pixdim['sagittal'][0], patch_size[0])
+            range_y = np.linspace(ind[1] - (patch_size[1] / 2.0) * patch_pixdim['sagittal'][1], ind[1] + (patch_size[1] / 2.0) * patch_pixdim['sagittal'][1], patch_size[1])
+            coord_x, coord_y = np.meshgrid(range_x, range_y)
+            coord_x = coord_x.ravel()
+            coord_y = coord_y.ravel()
+            coord_physical = [[ind[0], coord_x[i], coord_y[i]] for i in range(len(coord_x))]
+
+            for raw_image in raw_images:
+                grid_voxel = np.array(raw_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(raw_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                        interpolation_mode=1), (patch_size, patch_size))
+                patches_raw.append(np.expand_dims(patch, axis=0))
+
+            for gold_image in gold_images:
+                grid_voxel = np.array(gold_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(gold_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                         interpolation_mode=1), (patch_size, patch_size))
+                patches_gold.append(np.expand_dims(patch, axis=0))
+
+        if 'frontal' in patch_pixdim:
+            range_x = np.linspace(ind[0] - (patch_size[0] / 2.0) * patch_pixdim['frontal'][0], ind[0] + (patch_size[0] / 2.0) * patch_pixdim['frontal'][0], patch_size[0])
+            range_y = np.linspace(ind[1] - (patch_size[1] / 2.0) * patch_pixdim['frontal'][1], ind[1] + (patch_size[1] / 2.0) * patch_pixdim['frontal'][1], patch_size[1])
+            coord_x, coord_y = np.meshgrid(range_x, range_y)
+            coord_x = coord_x.ravel()
+            coord_y = coord_y.ravel()
+            coord_physical = [[coord_x[i], ind[1], coord_y[i]] for i in range(len(coord_x))]
+
+            for raw_image in raw_images:
+                grid_voxel = np.array(raw_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(raw_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                        interpolation_mode=1), (patch_size, patch_size))
+                patches_raw.append(np.expand_dims(patch, axis=0))
+
+            for gold_image in gold_images:
+                grid_voxel = np.array(gold_image.transfo_phys2continuouspix(coord_physical))
+                patch = np.reshape(gold_image.get_values(np.array([grid_voxel[:, 0], grid_voxel[:, 1], grid_voxel[:, 2]]),
+                                                         interpolation_mode=1), (patch_size, patch_size))
+                patches_gold.append(np.expand_dims(patch, axis=0))
+
+        test = np.concatenate(patches_raw, axis=0)
+        print test.shape
+
+        patches_raw.append(np.concatenate(patches_raw, axis=0))
+        patches_gold.append(np.concatenate(patches_gold, axis=0))
+
+    patches_raw = np.concatenate(patches_raw, axis=0)
+    patches_gold = np.concatenate(patches_gold, axis=0)
+    print patches_raw.shape
+
+    return patches_raw, patches_gold
 
 
 class FileManager():
@@ -135,7 +187,7 @@ class FileManager():
 
         x, y, z = np.mgrid[0:image_dim[0], 0:image_dim[1], 0:image_dim[2]]
         indexes = np.array(zip(x.ravel(), y.ravel(), z.ravel()))
-        physical_coordinates = image.transfo_pix2phys(indexes)
+        physical_coordinates = np.asarray(image.transfo_pix2phys(indexes))
 
         random_batch = np.random.choice(physical_coordinates.shape[0], int(round(physical_coordinates.shape[0] * self.ratio_patches_voxels)))
 
@@ -146,17 +198,19 @@ class FileManager():
         for i, fnames in enumerate(self.training_dataset):
             fname_raw_images = self.training_dataset[i][0]
             fname_gold_images = self.training_dataset[i][1]
-            reference_image = Image(fname_raw_images[0])  # first raw image is selected as reference
+            reference_image = Image(self.dataset_path + fname_raw_images[0])  # first raw image is selected as reference
+            print fname_raw_images[0]
 
             patches_coordinates = self.compute_patches_coordinates(reference_image)
 
-            patches = extract_patches_from_image(fname_raw_images,
-                                                 fname_gold_images,
-                                                 patches_coordinates,
-                                                 self.patch_info,
+            patches_raw, patches_gold = extract_patches_from_image(path_dataset=self.dataset_path,
+                                                 fname_raw_images=fname_raw_images,
+                                                 fname_gold_images=fname_gold_images,
+                                                 patches_coordinates=patches_coordinates,
+                                                 patch_info=self.patch_info,
                                                  verbose=1)
 
-            
+
 
         return
 
@@ -190,13 +244,11 @@ my_file_manager = FileManager(dataset_path='/Users/benjamindeleener/data/data_au
                               fct_explore_dataset=extract_list_file_from_path,
                               patch_extraction_parameters={'ratio_dataset': [0.6, 0.2, 0.2],
                                                            'patch_size': [32, 32],
-                                                           'patch_pixdim': [1.0, 1.0],
+                                                           'patch_pixdim': {'axial': [1.0, 1.0]},
                                                            'extract_all_positive': True,
                                                            'extract_all_negative': False},
                               fct_groundtruth_patch=None)
 
 training_dataset, testing_dataset, validation_dataset = my_file_manager.decompose_dataset()
-print training_dataset
-print testing_dataset
-print validation_dataset
+my_file_manager.explore()
 
