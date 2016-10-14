b4f75ba09134c57a78164dc63f7f5afb686316f6
Benjamin De Leener <jackbdl@gmail.com>
2016-10-14 17:32:35 -0400

    DEV: first attempt for can trainer script
    Former-commit-id: cf9fb926ac141b5e0c43e990bde5a0cb8c80ce14
    Former-commit-id: d6eacddab0ef1c03458f781e5fb923c644ab1316

---
+++ dev/train_patch_based_cnn.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+#########################################################################################
+#
+# This module is used to run msct_machine_learning.py
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2016 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Authors: Benjamin De Leener
+# Modified: 2016-10-04
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+
+import os
+import sct_utils as sct
+import numpy as np
+import json
+import pickle
+from skimage.feature import hog
+from hyperopt import tpe
+from sklearn.metrics import roc_auc_score, precision_score, recall_score, accuracy_score
+from sklearn.base import BaseEstimator
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+
+########## Change path
+import sys
+sys.path.insert(0, '/Users/chgroc/spinalcordtoolbox/scripts')
+from msct_machine_learning_cg import Trainer, FileManager
+
+# TODO: Rajouter center_of_patch_equal_one dans user case
+#       Mettre a jour File Manager + fct au dessus
+#       Faire 2 fichiers dans dev: CNN et SVM
+
+class Model(object):
+    def __init__(self, params):
+        self.params = params
+
+    def load(self, fname_in):
+        pass
+
+    def save(self, fname_out):
+        pass
+
+    def train(self, X, y):
+        pass
+
+    def predict(self, X):
+        return
+
+    def set_params(self, params):
+        pass
+
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD, Adadelta
+from keras.utils import np_utils
+
+class KerasConvNet(Sequential):
+    def __init__(self, params):
+        super(KerasConvNet, self).__init__()
+
+        if 'patch_size' in params:
+            self.patch_size = params['patch_size']  # must be a list of two elements
+        else:
+            self.patch_size = [32, 32]
+
+        if 'number_of_channels' in params:
+            self.number_of_channels = params['number_of_channels']
+        else:
+            self.number_of_channels = 1
+
+        if 'number_of_classes' in params:
+            self.number_of_classes = params['number_of_classes']
+        else:
+            self.number_of_classes = 2
+
+        if 'batch_size' in params:
+            self.batch_size = params['batch_size']
+        else:
+            self.batch_size = 256
+
+        if 'weight_class' in params:
+            self.weight_class = params['weight_class']
+        else:
+            self.weight_class = [1.0, 1.0]
+
+        # must be a list of list corresponding to the number of layers, depth and features.
+        # For example: [[32, 32], [64, 64]] means that there are two depth,
+        # two layers per depth and 32 and 64 features for each layer/depth, respectively
+        if 'depth_layers_features' in params:
+            self.number_of_layer_per_depth = params['number_of_features']
+        else:
+            self.number_of_layer_per_depth = [[32, 32], [64, 64]]
+
+        if 'number_of_feature_dense' in params:
+            self.number_of_feature_dense = params['number_of_feature_dense']
+        else:
+            self.number_of_feature_dense = 256
+
+        if 'activation_function' in params:
+            self.activation_function = params['activation_function']
+        else:
+            self.activation_function = 'relu'
+
+        if 'loss' in params:
+            self.loss = params['loss']
+        else:
+            self.loss = 'categorical_crossentropy'
+
+        self.create_model()
+
+    def create_model(self):
+        for d in range(len(self.number_of_layer_per_depth)):
+            for l in range(len(self.number_of_layer_per_depth[d])):
+                if d == 0 and l == 0:
+                    self.add(Convolution2D(self.number_of_layer_per_depth[d][l], 3, 3, border_mode='valid', input_shape=(self.number_of_channels, self.patch_size[0], self.patch_size[1])))
+                elif d != 0 and l == 0:
+                    self.add(Convolution2D(self.number_of_layer_per_depth[d][l], 3, 3, border_mode='valid'))
+                else:
+                    self.add(Convolution2D(self.number_of_layer_per_depth[d][l], 3, 3))
+                self.add(Activation(self.activation_function))
+            self.add(MaxPooling2D(pool_size=(2, 2)))
+            self.add(Dropout(0.25))
+
+        self.add(Flatten())
+        self.add(Dense(self.number_of_feature_dense))
+        self.add(Activation(self.activation_function))
+        self.add(Dropout(0.5))
+
+        self.add(Dense(self.number_of_classes))
+        self.add(Activation('softmax'))
+
+        ada = Adadelta(lr=1.0, rho=0.95, epsilon=1e-08)
+        self.compile(loss=self.loss, optimizer=ada)
+
+    def save(self, fname_out):
+        self.save_model(fname_out + '_model.h5')
+        self.save_weights(fname_out + '_weights.h5')
+
+    def load(self, fname_in):
+        self.load_weights(fname_in + '.h5')
+
+    def train(self, X, y):
+        self.train_on_batch(X, y, class_weight=self.weight_class)
+
+    def predict(self, X):
+        return super(KerasConvNet, self).predict(X, batch_size=self.batch_size)
+
+    def set_params(self, params):
+        if 'batch_size' in params:
+            self.batch_size = params['batch_size']
+        if 'depth_layers_features' in params:
+            self.number_of_layer_per_depth = params['number_of_features']
+        if 'number_of_feature_dense' in params:
+            self.number_of_feature_dense = params['number_of_feature_dense']
+        if 'activation_function' in params:
+            self.activation_function = params['activation_function']
+        if 'loss' in params:
+            self.loss = params['loss']
+        self.create_model()
+
+#########################################
+# USE CASE
+#########################################
+def extract_list_file_from_path(path_data):
+    ignore_list = ['.DS_Store']
+    sct.printv('Extracting ' + path_data)
+    cr = '\r'
+
+    list_data = []
+    for root, dirs, files in os.walk(path_data):
+        for fname_im in files:
+            if fname_im in ignore_list:
+                continue
+            if 'seg' in fname_im or 'gmseg' in fname_im:
+                continue
+            f_seg = None
+            for fname_seg in files:
+                if fname_im[:-7] in fname_seg:
+                    f_seg = fname_seg
+            list_data.append([[fname_im], [f_seg]])
+
+    return list_data
+
+
+def extract_patch_feature(im, param=None):
+    return im
+
+
+def center_of_patch_equal_one(data):
+    patch_size_x, patch_size_y = data['patches_gold'].shape[2], data['patches_gold'].shape[3]
+    return np.squeeze(data['patches_gold'][:, 0, int(patch_size_x / 2), int(patch_size_y / 2)])
+
+
+my_file_manager = FileManager(dataset_path='/Volumes/folder_shared-1/benjamin/machine_learning/patch_based/large_nobrain_nopad/',
+                              fct_explore_dataset=extract_list_file_from_path,
+                              patch_extraction_parameters={'ratio_dataset': [0.9, 0.1],
+                                                           'ratio_patches_voxels': 0.1,
+                                                           'patch_size': [32, 32],
+                                                           'patch_pixdim': {'axial': [1.0, 1.0]},
+                                                           'extract_all_positive': False,
+                                                           'extract_all_negative': False,
+                                                           'batch_size': 200},
+                              fct_groundtruth_patch=center_of_patch_equal_one)
+
+# my_file_manager.extract_all_positive = True
+# training_dataset, testing_dataset = my_file_manager.decompose_dataset(model_path)
+# my_file_manager.explore()
+
+results_path = '/Users/chgroc/data/spine_detection/results/'
+model_path = '/Users/chgroc/data/spine_detection/model/'
+data_path = '/Users/chgroc/data/spine_detection/data/'
+
+params_cnn = {'patch_size': [32, 32],
+              'number_of_channels': 1,
+              'batch_size': 128,
+              'number_of_features': [[32, 32], [64, 64]],
+              'loss': 'categorical_crossentropy'
+             }
+
+cnn_model = {'model_name': 'CNN', 'model': KerasConvNet(params_cnn),
+             'model_hyperparam': {'C': [1, 1000],
+                                  'kernel': ('sigmoid', 'poly', 'rbf'),
+                                  'gamma': [0, 20],
+                                  'class_weight': (None, 'balanced')}}
+
+methode_normalization_1={'methode_normalization_name':'histogram', 'param':{'cutoffp': (1, 99), 'landmarkp': [10, 20, 30, 40, 50, 60, 70, 80, 90], 'range': [0, 255]}}
+methode_normalization_2={'methode_normalization_name':'percentile', 'param':{'range': [0, 255]}}
+
+param_training = {'number_of_epochs': 50, 'patch_size': [32, 32], 'ratio_patch_per_img': 1.0,
+                    # 'minibatch_size_train': 500, 'minibatch_size_test': 500, # For CNN
+                    'hyperopt': {'algo':tpe.suggest, 'nb_eval':10, 'fct': roc_auc_score, 'nb_epoch': 1, 'eval_factor': 1}}
+
+### Attention .json et .pbz2 : modif a faire dans Trainer.__init__
+my_trainer = Trainer(data_filemanager_path=data_path,
+                     datasets_dict_fname='datasets.pbz2',
+                     patches_dict_prefixe='patches_coordinates_',
+                     patches_pos_dict_prefixe='patches_coordinates_positives_',
+                     classifier_model=cnn_model,
+                     fct_feature_extraction=extract_patch_feature,
+                     param_training=param_training,
+                     results_path=results_path,
+                     model_path=model_path)
+
+coord_prepared_train, label_prepared_train = my_trainer.prepare_patches(my_trainer.fname_training_raw_images)
+coord_prepared_test, label_prepared_test = my_trainer.prepare_patches(my_trainer.fname_testing_raw_images)
+
+my_trainer.hyperparam_optimization(coord_prepared_train, label_prepared_train, ratio_test=0.4)
+my_trainer.set_hyperopt_train(coord_prepared_train, label_prepared_train)
+# my_trainer.set_hyperopt_train(coord_prepared_train, label_prepared_train, results_path + 'best_trial.pkl')
+my_trainer.run_prediction(coord_prepared_test, label_prepared_test)
