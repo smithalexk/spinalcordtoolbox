417766dc9d1778f37d5af2fdea67ff4ea0ddbe05
Benjamin De Leener <jackbdl@gmail.com>
2016-10-16 09:18:15 -0400

    DEV: fixed names
    Former-commit-id: d0d0483434eebc2edc7e07c68170864ff4557488
    Former-commit-id: a0168edaadab4134771f3249ad32176e3cfef6da

--- scripts/msct_machine_learning.py
+++ scripts/msct_machine_learning.py
@@ -431,8 +431,9 @@ def extract_list_file_from_path(path_data):
                 continue
             f_seg = None
             for fname_seg in files:
-                if fname_im[:-7] in fname_seg:
-                    f_seg = fname_seg
+                if 'seg' in fname_im or 'gmseg' in fname_seg:
+                    if fname_im[:-7] in fname_seg:
+                        f_seg = fname_seg
             list_data.append([[fname_im], [f_seg]])
 
     return list_data
